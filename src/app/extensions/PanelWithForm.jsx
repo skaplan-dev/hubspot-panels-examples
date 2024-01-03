f8ea5917303fd2caef1f17cb975b1b7cfcf8acf0
Seth Kaplan <skaplan@hubspot.com>
2024-01-03 11:53:54 -0500

    add new card

---
+++ src/app/extensions/PanelWithForm.jsx
@@ -0,0 +1,88 @@
+import React from 'react'
+import {
+  Button,
+  Flex,
+  hubspot,
+  Link,
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+  Text,
+} from '@hubspot/ui-extensions'
+import { Panel } from '@hubspot/ui-extensions/experimental'
+
+const specs = [
+  {
+    name: 'Capacity',
+    value: '128GB',
+  },
+  {
+    name: 'Display',
+    value: '6.7-inch (diagonal) all-screen OLED display',
+  },
+  {
+    name: 'Chip',
+    value: 'A15 Bionic chip',
+  },
+  {
+    name: 'Camera',
+    value: 'Pro 12MP camera system: Telephoto, Wide, and Ultra Wide cameras',
+  },
+  {
+    name: 'Video Recording',
+    value: '4K video recording at 24 fps, 25 fps, 30 fps, or 60 fps',
+  },
+  {
+    name: 'TrueDepth Camera',
+    value: '12MP camera',
+  },
+  {
+    name: 'Battery',
+    value: 'Built-in rechargeable lithium-ion battery',
+  },
+  {
+    name: 'Water Resistant',
+    value:
+      'Rated IP68 (maximum depth of 6 meters up to 30 minutes) under IEC standard 60529',
+  },
+]
+
+// Define the extension to be run within the Hubspot CRM
+hubspot.extend(({ runServerlessFunction, actions }) => (
+  <Extension runServerlessFunction={runServerlessFunction} actions={actions} />
+))
+
+const Extension = () => {
+  return (
+    <>
+      <Panel
+        id='my-panel'
+        title={`IPhone 14 specs`}
+        onClose={() => console.log('closed')}
+        flush={true}
+      >
+        <Table flush={true}>
+          <TableHead>
+            <TableRow>
+              <TableHeader>Spec</TableHeader>
+              <TableHeader>Value</TableHeader>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {specs.map(spec => (
+              <TableRow>
+                <TableCell>{spec.name}</TableCell>
+                <TableCell>{spec.value}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </Panel>
+
+      <Button onClick={() => console.log('clicked')}>Click me</Button>
+    </>
+  )
+}
