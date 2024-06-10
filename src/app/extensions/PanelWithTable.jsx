5db3142be0bf8d0d9d2bbcae4ddb85cbf66f09cd
Seth Kaplan <skaplan@hubspot.com>
2024-06-10 14:26:53 -0400

    teehee

--- src/app/extensions/PanelWithTable.jsx
+++ src/app/extensions/PanelWithTable.jsx
@@ -1,21 +1,11 @@
-import React from 'react'
 import {
   Button,
-  Flex,
-  Form,
-  hubspot,
-  Input,
-  Link,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-  Text,
+  Modal,
+  ModalBody,
+  Panel, PanelSection,
+  hubspot
 } from '@hubspot/ui-extensions'
-import { Panel, PanelSection, PanelBody } from '@hubspot/ui-extensions'
-import {CrmAssociationTable} from '@hubspot/ui-extensions/crm'
+import React from 'react'
 
 const specs = [
   {
@@ -53,107 +43,30 @@ const specs = [
   },
 ]
 
-// Define the extension to be run within the Hubspot CRM
 hubspot.extend(({ runServerlessFunction, actions }) => (
   <Extension runServerlessFunction={runServerlessFunction} actions={actions} />
 ))
 
-const Modal = 'Modal'
-const ModalBody = 'ModalBody'
-
-const ModalFooter = 'ModalFooter'
-
-const Extension = () => {
+const PanelComponent = () => {
+  return (
+    <Panel>
+      <PanelSection>test 1234</PanelSection>
+    </Panel>
+  )
+}
+const Extension = ({ actions }) => {
   return (
     <>
-      <Modal
-        id='my-panel'
-        title='Delete domain?'
-        onClose={() => console.log('closed')}
-        variant="danger"
+      <Button
+        overlay={
+          <Modal id='1234'>
+            <ModalBody>test 123</ModalBody>
+          </Modal>
+        }
       >
-        <ModalBody>
-        You're about to delete the domain <Text format={{fontWeight : 'bold'}} inline={true}>www.examplewebsite.com</Text>. This can't be undone.
-        </ModalBody>
-        {/* <ModalFooter>
-          <Button variant='primary'>Delete domain</Button>
-          <Button onClick={(_, reactions) => reactions.openPanel('1234')}>
-Cancel
-</Button>
-        </ModalFooter> */}
-      </Modal>
-      <CrmAssociationTable
-        objectTypeId="0-3"
-        propertyColumns={[
-          'dealname',
-          'hubspot_owner_id',
-          'amount',
-          'dealstage',
-        ]}
-        quickFilterProperties={['hubspot_owner_id', 'dealstage', 'amount']}
-        pageSize={10}
-        preFilters={[
-          {
-            operator: 'NOT_IN',
-            property: 'dealstage',
-            values: ['closedwon', 'closedlost'],
-          },
 
-        ]}
-        sort={[
-          {
-            direction: 1,
-            columnName: 'amount',
-          },
-        ]}
-        searchable={true}
-        pagination={true}
-      />
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableHeader>Item</TableHeader>
-            <TableHeader>Price</TableHeader>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          <TableRow>
-            <TableCell>IPhone 14</TableCell>
-            <TableCell>
-              <Flex align='center' direction='row' justify='between'>
-                <Text>$1000</Text>
-                <Button
-                  size='xs'
-                  onClick={async (__, reactions) => {
-                    reactions.openModal('my-panel')
-                  }}
-                >
-                  Open Modal
-                </Button>
-                <Button
-                  size='xs'
-                  onClick={async (__, reactions) => {
-                    reactions.openPanel('1234')
-                  }}
-                >
-                  Open Panel
-                </Button>
-              </Flex>
-            </TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-      <Panel id='1234'>
-
-        <Button
-          size='xs'
-          onClick={async (__, reactions) => {
-            reactions.openModal('my-panel')
-          }}
-        >
-          See Specs
-        </Button>
-      </Panel>
+        open modal
+      </Button>
     </>
   )
 }
